665a08c9994e1fdc05673ff41516166dc9634ad5
Joseph Anson <josephanson@hotmail.co.uk>
2022-02-09 19:30:20 +0100

    style: changed styling for select so placeholder won't pop up anymore

--- modules/phones/components/PhoneFilter.vue
+++ modules/phones/components/PhoneFilter.vue
@@ -3,15 +3,15 @@
     <div v-for="(filter, key) of filterConfig" :key="key" class="mr-2 filter">
       <div class="group relative">
         <select
-          class="w-full border border-gray-300 bg-white py-2 pl-3 pr-10 text-left focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+          class="pointer-events-none w-full border border-gray-300 bg-white py-2 pl-3 pr-10 text-left focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
         >
-          <option disabled selected>
+          <option disabled selected hidden>
             {{ $i18n.t(`phone.filter.${key}`) }}
           </option>
         </select>
 
         <div
-          class="absolute left-0 top-full z-10 hidden rounded-md bg-white py-2 px-4 text-base shadow-lg shadow-gray-800 transition ease-in-out group-hover:inline-block sm:text-sm"
+          class="absolute left-0 top-full z-10 hidden rounded-md bg-white py-2 px-4 text-base shadow-lg shadow-gray-800 transition ease-in-out group-hover:inline-block group-focus:inline-block sm:text-sm"
         >
           <MultiSelect
             :value="newValue[key]"
